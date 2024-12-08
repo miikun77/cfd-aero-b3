4e6b45e92ed66dd3b6504fe295aab37b7acbf7cc
miikun77 <64695370+miikun77@users.noreply.github.com>
2024-12-08 09:20:51 +0900

    fsmach, alphaを計算結果に

--- main.py
+++ main.py
@@ -78,6 +78,8 @@ def create_mesh_d(fsmach, alpha):
         file.write("   n_steps=20000, cdt=0.9, eps4=20.,  jtail=17\n")
         file.write(" &END\n")
 
+
+
 def execute_euler():
     process = subprocess.Popen(["./build/2deuler"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     stdout, stderr = [], []
@@ -108,21 +110,26 @@ def execute_euler():
             "Cl_surface": float(cl_surface.group(1)) if cl_surface else None,
             "Cd_surface": float(cd_surface.group(1)) if cd_surface else None
         }
-        
-        # CSVに保存
-        with open("result.csv", "w", newline='') as csvfile:
-            fieldnames = ["Cl_integration", "Cl_surface", "Cd_surface"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            
-            writer.writeheader()
-            writer.writerow(results)
+        return results
     else:
         print("Euler execution failed")
         print(''.join(stderr))
 
+def save_results_to_csv(fsmach, alpha, results):
+    results.update({"fsmach": fsmach, "alpha": alpha})
+    with open("result.csv", "w", newline='') as csvfile:
+        fieldnames = ["fsmach", "alpha", "Cl_integration", "Cl_surface", "Cd_surface"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        writer.writerow(results)
+
 if __name__ == "__main__":
     create_directories()
-    create_mesh_d(fsmach=0.800, alpha=1.25)
+    fsmach = 0.800
+    alpha = 1.25
+    create_mesh_d(fsmach=fsmach, alpha=alpha)
     add_snow_effect(parameter=0.05)
     compile_mesh()
-    execute_euler()
+    results = execute_euler()
+    save_results_to_csv(fsmach=fsmach, alpha=alpha, results=results)
